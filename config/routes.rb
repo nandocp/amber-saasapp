f621e8aac0f6dfb238b3d275763d819370c6605c
Nando CP <fcpontes@gmail.com>
2017-03-14 16:39:02 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
-  get 'contact-unicorn', to: 'contacts#new'
+  resources :contacts, only: :create
+  get 'contact-unicorn', to: 'contacts#new', as: 'new_contact'
 end
